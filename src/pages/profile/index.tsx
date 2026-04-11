ef96c71518c553c884351c530e2480ccb98f3549
Arnanz <pashamaulana14@gmail.com>
2026-04-11 22:27:45 +0700

    Jobsheet 14

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,4 +1,13 @@
-const ProfilePage = () => {
-  return <div>Halaman Profile Utama</div>;
-};
-export default ProfilePage;
+import { useSession } from "next-auth/react";
+
+const HalamanProfile = () => {
+  const {data}:any = useSession();
+  return (
+    <div>
+      <h1>Halaman Profile</h1><br />
+      <h1>Selamat Datang {data?.user?.fullname}</h1>
+    </div>
+  )
+}
+
+export default HalamanProfile
